6e8ae6f24f821e26ae5ec0cac57e26b82044de96
Zicka67 <art.book@hotmail.fr>
2023-09-17 17:19:21 +0200

    Mise en place des fixtures pour les users, photos et produits

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -57,7 +57,7 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
     public function __construct()
     {
         $this->orders = new ArrayCollection();
-        // $this->created_at = new \DateTimeImmutable();
+        $this->created_at = new \DateTimeImmutable();
     }
 
     public function getId(): ?int
