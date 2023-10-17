480d916bc4a479c79adada5dbdf1af9dc9f99f4c
Zicka67 <Art.book@hotmail.fr>
2023-10-17 11:26:56 +0200

    Correction de la requete AJAX

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,14 +1,14 @@
-function updateCart(actionUrl) {
-    // Envoie une requête HTTP GET à l'URL spécifiée (actionUrl)
-    fetch(actionUrl)
-        // Une fois que la requête est complète, convertit la réponse en objet JSON
-        .then(response => response.json())
-        // Utilise les données JSON pour update le contenu de l'élément HTML avec l'id 'totalItems'
-        .then(data => {
-            document.getElementById('totalItems').textContent = data.totalItems;
-        })
-        // Si une erreur se produit, elle est gardée dans la console
-        .catch(error => console.error('Error:', error));
+// function updateCart(actionUrl) {
+//     // Envoie une requête HTTP GET à l'URL spécifiée (actionUrl)
+//     fetch(actionUrl)
+//         // Une fois que la requête est complète, convertit la réponse en objet JSON
+//         .then(response => response.json())
+//         // Utilise les données JSON pour update le contenu de l'élément HTML avec l'id 'totalItems'
+//         .then(data => {
+//             document.getElementById('totalItems').textContent = data.totalItems;
+//         })
+//         // Si une erreur se produit, elle est gardée dans la console
+//         .catch(error => console.error('Error:', error));
 
-    }
+//     }
 
